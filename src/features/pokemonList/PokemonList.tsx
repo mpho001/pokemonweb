f7061db9685edd8538a0ac0e9bb69336973cdd60
Melonderr <mpho001@ucr.edu>
2021-09-08 21:42:54 -0700

    set up typescript, webpack, babel

---
+++ src/features/pokemonList/PokemonList.tsx
@@ -0,0 +1,20 @@
+/// import React, { useState } from 'react';
+
+// import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import styles from '../counter/Counter.module.css';
+
+export function PokemonList() {
+//   const count = useAppSelector(selectCount);
+//   const dispatch = useAppDispatch();
+//   const [incrementAmount, setIncrementAmount] = useState('2');
+
+//   const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <span className={styles.value}>Hi</span>
+      </div>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    publicPath: "/",
+    contentBase: "./src",
+    hot: true
+},
+  entry: './src/index.tsx',
+  mode: "development",
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
+        use: ["babel-loader"] 
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg|ico)$/,
+        use: ["file-loader"],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, "./public", "index.html"),
+        filename: './index.html',
+        favicon: './public/favicon.ico',
+        manifest: "./public/manifest.json"
+    }),
+],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
